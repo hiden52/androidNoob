03b0c8de46dc4bbc8c908bde68aa4e6fd51cc78d
hiden52 <zerorequiem23@gmail.com>
2021-04-01 14:36:42 +0000

    Learn about class

--- Typescript/typechain/dist/index.js
+++ Typescript/typechain/dist/index.js
@@ -1,12 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "Lizzy",
-    age: 21,
-    gender: "female"
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+// interface Human {
+//     name:string;
+//     age:number;
+//     gender:string;
+// }
+// const person = {
+//     name: "Lizzy",
+//     age: 21,
+//     gender: "female"
+// }
+const lynn = new Human("Lynn", 22, "female");
 const sayHi = (person) => {
     return (`Hello ${person.name}, you are ${person.age}, you are ${person.gender}`);
 };
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
--- Typescript/typechain/src/index.ts
+++ Typescript/typechain/src/index.ts
@@ -1,19 +1,32 @@
-interface Human {
-    name:string;
-    age:number;
-    gender:string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender?: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "Lizzy",
-    age: 21,
-    gender: "female"
-}
+// interface Human {
+//     name:string;
+//     age:number;
+//     gender:string;
+// }
+
+// const person = {
+//     name: "Lizzy",
+//     age: 21,
+//     gender: "female"
+// }
+
+const lynn = new Human("Lynn", 22, "female");
 
 const sayHi = (person: Human): string => {
     return (`Hello ${person.name}, you are ${person.age}, you are ${person.gender}`);
 }
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export { }
