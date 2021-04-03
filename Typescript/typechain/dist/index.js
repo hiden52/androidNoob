73649166fcf36d2eb094171b6ab6eb0755e7f276
hiden52 <zerorequiem23@gmail.com>
2021-04-03 14:53:31 +0000

    Finish making a blockchain with Typescript

--- Typescript/typechain/dist/index.js
+++ Typescript/typechain/dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,8 +10,48 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+Block.calculateBockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+Block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
 const genesisBlock = new Block(0, "sflehfi", "", "Hiyo~", 123456);
-let blockchaine = [genesisBlock];
-blockchaine.push(new Block(1, "dhfiefhi", "sflehfi", "Long Long stick", 23231));
-console.log(blockchaine);
+let blockchain = [genesisBlock];
+const getBlockchain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimestamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimestamp();
+    const newHash = Block.calculateBockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    addBlock(newBlock);
+    //console.log(newBlock);
+    return newBlock;
+};
+const getHashForBlock = (aBlock) => Block.calculateBockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+const isBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock))
+        return false;
+    else if (previousBlock.index + 1 !== candidateBlock.index)
+        return false;
+    else if (previousBlock.hash !== candidateBlock.previousHash)
+        return false;
+    else if (getHashForBlock(candidateBlock) !== candidateBlock.hash)
+        return false;
+    else
+        return true;
+};
+const addBlock = (candidateBlock) => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+    //console.log(isBlockValid(candidateBlock, getLatestBlock()));
+};
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- Typescript/typechain/src/index.ts
+++ Typescript/typechain/src/index.ts
@@ -1,29 +1,102 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
-    constructor(
-        index: number,
-        hash: string,
-        previousHash: string,
-        data: string,
-        timestamp: number
-    ) {
-        this.index = index;
-        this.hash = hash;
-        this.previousHash = previousHash;
-        this.data = data;
-        this.timestamp = timestamp;
-    }
+	public index: number;
+	public hash: string;
+	public previousHash: string;
+	public data: string;
+	public timestamp: number;
+
+	static calculateBockHash = (
+		index: number,
+		previousHash: string,
+		timestamp: number,
+		data: string
+	): string =>
+		CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+	static validateStructure = (aBlock: Block): boolean =>
+		typeof aBlock.index === "number" &&
+		typeof aBlock.hash === "string" &&
+		typeof aBlock.previousHash === "string" &&
+		typeof aBlock.timestamp === "number" &&
+		typeof aBlock.data === "string";
+
+	constructor(
+		index: number,
+		hash: string,
+		previousHash: string,
+		data: string,
+		timestamp: number
+	) {
+		this.index = index;
+		this.hash = hash;
+		this.previousHash = previousHash;
+		this.data = data;
+		this.timestamp = timestamp;
+	}
 }
 
 const genesisBlock: Block = new Block(0, "sflehfi", "", "Hiyo~", 123456);
 
-let blockchaine: [Block] = [genesisBlock];
-blockchaine.push(new Block(1, "dhfiefhi", "sflehfi", "Long Long stick" , 23231));
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchaine);
+const getBlockchain = (): Block[] => blockchain;
 
-export { }
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+	const previousBlock: Block = getLatestBlock();
+	const newIndex: number = previousBlock.index + 1;
+	const newTimestamp: number = getNewTimestamp();
+	const newHash: string = Block.calculateBockHash(
+		newIndex,
+		previousBlock.hash,
+		newTimestamp,
+		data
+	);
+	const newBlock: Block = new Block(
+		newIndex,
+		newHash,
+		previousBlock.hash,
+		data,
+		newTimestamp
+	);
+	addBlock(newBlock);
+	//console.log(newBlock);
+	return newBlock;
+};
+
+const getHashForBlock = (aBlock: Block): string =>
+	Block.calculateBockHash(
+		aBlock.index,
+		aBlock.previousHash,
+		aBlock.timestamp,
+		aBlock.data
+	);
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+	if (!Block.validateStructure(candidateBlock)) return false;
+	else if (previousBlock.index + 1 !== candidateBlock.index) return false
+	else if (previousBlock.hash !== candidateBlock.previousHash) return false;
+	else if (getHashForBlock(candidateBlock) !== candidateBlock.hash)
+		return false;
+	else return true;
+};
+
+const addBlock = (candidateBlock: Block): void => {
+	if (isBlockValid(candidateBlock, getLatestBlock())) {
+		blockchain.push(candidateBlock);
+	}
+	//console.log(isBlockValid(candidateBlock, getLatestBlock()));
+};
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
+
+export {};
